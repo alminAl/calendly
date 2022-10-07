4e2eb05a07712ed0968d981abbd2e5f51506823a
Istiak Ahmed <istiakahmed104127@gmail.com>
2022-10-07 18:26:01 +0600

    home component update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../components/HomeComponent/Header/Header'
+import Schedule from '../components/HomeComponent/Schedule/Schedule'
 import Solution from '../components/HomeComponent/Solution/Solution'
+
+
 
 const Home = () => {
   return (
    <div>
      <Header/>
+     <Schedule/>
      <Solution/>
    </div>
   )
